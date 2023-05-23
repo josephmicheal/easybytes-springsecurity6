4a05444de9152c210c7bba796f07930f65663914
josephmicheal <sun.leo04@gmail.com>
2023-05-23 09:49:31 +0530

    updated for bcrypt login password encoding

---
+++ section4/springsecsection4v1/src/main/java/com/eazybytes/EazyBCryptApplication.java
@@ -0,0 +1,13 @@
+package com.eazybytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyBCryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyBCryptApplication.class, args);
+	}
+
+}
